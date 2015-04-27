976a2809b8b9ee848488bd0dc50a30ef7dfc41f4
Bryce Summers <bwsummer@unix3.andrew.cmu.edu>
2015-04-27 01:01:26 -0400

    Code is reay for debugging.

--- Mazes/main.cpp
+++ Mazes/main.cpp
@@ -6,6 +6,12 @@
 #include "include/Tester.h"
 
 
+
+// Used for timing.
+#include <ctime>
+#include <unistd.h>
+
+
 using namespace std;
 
 
@@ -21,6 +27,9 @@ using namespace std;
 
 void test_maze_implementations(Tester * TEST)
 {
+
+  cout << "Testing an implementation.";
+
   Maze_ADT * maze = new Maze_2dLattice();
   TEST -> test(maze);
   delete maze;
@@ -29,7 +38,6 @@ void test_maze_implementations(Tester * TEST)
   maze = Maze
 
   */
-
 
 }
 
@@ -55,23 +63,38 @@ void maze_serial(Tester * TEST)
     cout << TEST->test(maze, UF);
 }
 
-
-
 int main()
 {
+
+   cout<< "Program has started." << endl;
+
 
    Tester * TEST = new Tester();
 
 #if defined(DEBUG)
+
+  cout<< "Testing the implementations" << endl;
   test_implementations(TEST);
+  cout << "Implementations Passed Unit Tests." << endl;
 #endif
 
 
-    maze_serial(TEST);
 
+	cout << "Starting the maze testing." << endl;
+
+    clock_t a = clock();
+    maze_serial(TEST);
+	clock_t b = clock();
+	cout << "Maze Time = " << (b - a) << endl;
+
+
+	cout << "Clean up." << endl;
 
 	// Clean up memory.
 	delete TEST;
+
+
+	cout<< "Exiting. Have a nice day." << endl;
 
     return 0;
 }
